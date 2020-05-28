975ff9b58bbb7d277a014b5b636e4b45b537cc35
Costin Somodea <costin@teamcode.ro>
2020-05-28 18:50:02 +0300

    video tile work in progress

--- DatingApp-SPA/src/app/meetings/meetings.component.ts
+++ DatingApp-SPA/src/app/meetings/meetings.component.ts
@@ -12,7 +12,8 @@ import {
   MeetingSessionConfiguration,
   AudioVideoFacade,
   DefaultAudioMixController,
-  TimeoutScheduler
+  TimeoutScheduler,
+  VideoTileState
 } from 'amazon-chime-sdk-js';
 
 @Component({
@@ -35,6 +36,7 @@ export class MeetingsComponent implements OnInit {
   // show/hide divs
   showMeetingForm = true;
   showSelectDevicesForm = false;
+  showMeetingScreen = false;
 
   // aws chime sdk objects
   logger = new ConsoleLogger('MyLogger', LogLevel.INFO);
@@ -60,6 +62,70 @@ export class MeetingsComponent implements OnInit {
 
   audioVideo: AudioVideoFacade | null = null;
 
+  // enable/disable mic, video, screen sharing
+  isMicrophoneEnabled = true;
+  isCameraEnabled = false;
+  activeSpeakerLayout = true;
+  tileOrganizer: DemoTileOrganizer = new DemoTileOrganizer();
+  tileIndexToTileId: { [id: number]: number } = {};
+  tileIdToTileIndex: { [id: number]: number } = {};
+
+  // Add an observer to receive session lifecycle events: connecting, start, and stop
+  observer = {
+    audioVideoDidStart: () => {
+      console.log('Started audio video');
+    },
+    audioVideoDidStop: sessionStatus => {
+      // See the "Stopping a session" section for details.
+      console.log('Stopped with a session status code: ', sessionStatus.statusCode());
+    },
+    audioVideoDidStartConnecting: reconnecting => {
+      if (reconnecting) {
+        // e.g. the WiFi connection is dropped.
+        console.log('Attempting to reconnect');
+      }
+    },
+    // videoTileDidUpdate is called whenever a new tile is created or tileState changes.
+    videoTileDidUpdate: (tileState: VideoTileState) => {
+      console.log(`video tile updated: ${JSON.stringify(tileState, null, '  ')}`);
+      const tileIndex = tileState.localTile
+      ? 16
+      : this.tileOrganizer.acquireTileIndex(tileState.tileId);
+      const tileElement = document.getElementById(`tile-${tileIndex}`) as HTMLDivElement;
+      const videoElement = document.getElementById(`video-${tileIndex}`) as HTMLVideoElement;
+      const nameplateElement = document.getElementById(`nameplate-${tileIndex}`) as HTMLDivElement;
+
+      const pauseButtonElement = document.getElementById(`video-pause-${tileIndex}`) as HTMLButtonElement;
+      const resumeButtonElement = document.getElementById(`video-resume-${tileIndex}`) as HTMLButtonElement;
+      pauseButtonElement.addEventListener('click', () => {
+        if (!tileState.paused) {
+          this.audioVideo.pauseVideoTile(tileState.tileId);
+        }
+      });
+      resumeButtonElement.addEventListener('click', () => {
+        if (tileState.paused) {
+          this.audioVideo.unpauseVideoTile(tileState.tileId);
+        }
+      });
+
+      console.log(`binding video tile ${tileState.tileId} to ${videoElement.id}`);
+      this.audioVideo.bindVideoElement(tileState.tileId, videoElement);
+      this.tileIndexToTileId[tileIndex] = tileState.tileId;
+      this.tileIdToTileIndex[tileState.tileId] = tileIndex;
+      // TODO: enforce roster names
+      // new TimeoutScheduler(200).start(() => {
+      //   const rosterName = this.roster[tileState.boundAttendeeId]
+      //     ? this.roster[tileState.boundAttendeeId].name
+      //     : '';
+      //   if (nameplateElement.innerHTML !== rosterName) {
+      //     nameplateElement.innerHTML = rosterName;
+      //   }
+      // });
+      tileElement.style.display = 'block';
+      this.layoutVideoTiles();
+    }
+  };
+
   constructor(private chimeService: ChimeService) { }
 
   ngOnInit() {
@@ -78,6 +144,7 @@ export class MeetingsComponent implements OnInit {
 
   createAttendee = (meetingId: string) => {
     this.chimeService.createAttendee(this.meeting.meetingId).subscribe((attendeeResponse: AttendeeResponse) => {
+      this.attendee = attendeeResponse.attendee;
       this.jsonAttendeeResponse = JSON.stringify(attendeeResponse);
       this.showMeetingForm = false;
       this.showSelectDevicesForm = true;
@@ -207,6 +274,65 @@ export class MeetingsComponent implements OnInit {
     new TestSound(audioOutput.value);
   }
 
+  joinMeeting = () => {
+    console.log('join meeting');
+
+    // stop video preview
+    const videoElement = document.getElementById('video-preview') as HTMLVideoElement;
+    this.audioVideo.stopVideoPreviewForVideoInput(videoElement);
+
+    this.showSelectDevicesForm = false;
+    this.showMeetingScreen = true;
+
+    const audioElement = document.getElementById('meeting-audio') as HTMLAudioElement;
+    this.meetingSession.audioVideo.bindAudioElement(audioElement);
+    this.meetingSession.audioVideo.addObserver(this.observer);
+    this.observer.audioVideoDidStart();
+  }
+
+  // mute unmute microphone
+  toggleMicrophone = () => {
+    this.isMicrophoneEnabled = !this.isMicrophoneEnabled;
+    console.log('toggle mic', this.isMicrophoneEnabled);
+
+    if(this.isMicrophoneEnabled)
+    {
+      const unmuted = this.meetingSession.audioVideo.realtimeUnmuteLocalAudio();
+      if (unmuted) {
+        console.log('Other attendees can hear your audio');
+      } else {
+        // See the realtimeSetCanUnmuteLocalAudio use case below.
+        console.log('You cannot unmute yourself');
+      }
+    }
+    else
+    {
+      this.meetingSession.audioVideo.realtimeMuteLocalAudio();
+    }
+  }
+
+  // enable disable camera
+  toggleCamera = async () => {
+    this.isCameraEnabled = !this.isCameraEnabled;
+    console.log('toggle camera', this.isCameraEnabled);
+    if(this.isCameraEnabled)
+    {
+      await this.meetingSession.audioVideo.chooseVideoInputDevice(this.selectedVideoInputDeviceId);
+
+      // const videoObserver = { };
+
+      // this.meetingSession.audioVideo.addObserver(videoObserver);
+
+      const startedLocalVideoTile = this.audioVideo.startLocalVideoTile();
+
+      console.log('startedLocalVideoTile:', startedLocalVideoTile);
+    }
+    else
+    {
+      console.log('stop video');
+    }
+  }
+
   private getInputDevicePermission = async (deviceId: string, deviceType: string) => {
     switch(deviceType){
       case 'audioInput':
@@ -272,6 +398,227 @@ export class MeetingsComponent implements OnInit {
     const htmlVideoPreviewElement = document.getElementById('video-preview') as HTMLVideoElement;
     this.audioVideo.startVideoPreviewForVideoInput(htmlVideoPreviewElement);
   }
+
+  layoutVideoTiles(): void {
+    if (!this.meetingSession) {
+      return;
+    }
+    const selfAttendeeId = this.meetingSession.configuration.credentials.attendeeId;
+    const selfTileId = this.tileIdForAttendeeId(selfAttendeeId);
+    const visibleTileIndices = this.visibleTileIndices();
+    let activeTileId = this.activeTileId();
+    const selfIsVisible = visibleTileIndices.includes(this.tileIdToTileIndex[selfTileId]);
+    if (visibleTileIndices.length === 2 && selfIsVisible) {
+      activeTileId = this.tileIndexToTileId[
+        visibleTileIndices[0] === selfTileId ? visibleTileIndices[1] : visibleTileIndices[0]
+      ];
+    }
+    const hasVisibleActiveSpeaker = visibleTileIndices.includes(
+      this.tileIdToTileIndex[activeTileId]
+    );
+    if (this.activeSpeakerLayout && hasVisibleActiveSpeaker) {
+      this.layoutVideoTilesActiveSpeaker(visibleTileIndices, activeTileId);
+    } else {
+      this.layoutVideoTilesGrid(visibleTileIndices);
+    }
+  }
+
+  tileIdForAttendeeId(attendeeId: string): number | null {
+    for (const tile of this.audioVideo.getAllVideoTiles()) {
+      const state = tile.state();
+      if (state.boundAttendeeId === attendeeId) {
+        return state.tileId;
+      }
+    }
+    return null;
+  }
+
+  activeTileId(): number | null {
+    // to do
+    // for (const attendeeId in this.roster) {
+    //   if (this.roster[attendeeId].active) {
+            // return this.tileIdForAttendeeId(attendeeId);
+    //   }
+    // }
+    // return null;
+
+    return this.tileIdForAttendeeId(this.attendee.attendeeId);
+  }
+
+  visibleTileIndices(): number[] {
+    let tiles: number[] = [];
+    const screenViewTileIndex = 17;
+    for (let tileIndex = 0; tileIndex <= screenViewTileIndex; tileIndex++) {
+      const tileElement = document.getElementById(`tile-${tileIndex}`) as HTMLDivElement;
+      if (tileElement.style.display === 'block') {
+        if (tileIndex === screenViewTileIndex) {
+          // Hide videos when viewing screen
+          for (const tile of tiles) {
+            const tileToSuppress = document.getElementById(`tile-${tile}`) as HTMLDivElement;
+            tileToSuppress.style.visibility = 'hidden';
+          }
+          tiles = [screenViewTileIndex];
+        } else {
+          tiles.push(tileIndex);
+        }
+      }
+    }
+    return tiles;
+  }
+
+  layoutVideoTilesGrid(visibleTileIndices: number[]): void {
+    const tileArea = document.getElementById('tile-area') as HTMLDivElement;
+    const width = tileArea.clientWidth;
+    const height = tileArea.clientHeight;
+    const widthToHeightAspectRatio = 16 / 9;
+    let columns = 1;
+    let totalHeight = 0;
+    let rowHeight = 0;
+    for (; columns < 18; columns++) {
+      const rows = Math.ceil(visibleTileIndices.length / columns);
+      rowHeight = width / columns / widthToHeightAspectRatio;
+      totalHeight = rowHeight * rows;
+      if (totalHeight <= height) {
+        break;
+      }
+    }
+    for (let i = 0; i < visibleTileIndices.length; i++) {
+      const w = Math.floor(width / columns);
+      const h = Math.floor(rowHeight);
+      const x = (i % columns) * w;
+      const y = Math.floor(i / columns) * h + (height / 2 - totalHeight / 2);
+      this.updateTilePlacement(visibleTileIndices[i], x, y, w, h);
+    }
+  }
+
+  updateTilePlacement(tileIndex: number, x: number, y: number, w: number, h: number): void {
+    const tile = document.getElementById(`tile-${tileIndex}`) as HTMLDivElement;
+    const insetWidthSize = 4;
+    const insetHeightSize = insetWidthSize / (16 / 9);
+    tile.style.position = 'absolute';
+    tile.style.left = `${x + insetWidthSize}px`;
+    tile.style.top = `${y + insetHeightSize}px`;
+    tile.style.width = `${w - insetWidthSize * 2}px`;
+    tile.style.height = `${h - insetHeightSize * 2}px`;
+    tile.style.margin = '0';
+    tile.style.padding = '0';
+    tile.style.visibility = 'visible';
+    const video = document.getElementById(`video-${tileIndex}`) as HTMLDivElement;
+    if (video) {
+      video.style.position = 'absolute';
+      video.style.left = '0';
+      video.style.top = '0';
+      video.style.width = `${w}px`;
+      video.style.height = `${h}px`;
+      video.style.margin = '0';
+      video.style.padding = '0';
+      video.style.borderRadius = '8px';
+    }
+    const nameplate = document.getElementById(`nameplate-${tileIndex}`) as HTMLDivElement;
+    const nameplateSize = 24;
+    const nameplatePadding = 10;
+    nameplate.style.position = 'absolute';
+    nameplate.style.left = '0px';
+    nameplate.style.top = `${h - nameplateSize - nameplatePadding}px`;
+    nameplate.style.height = `${nameplateSize}px`;
+    nameplate.style.width = `${w}px`;
+    nameplate.style.margin = '0';
+    nameplate.style.padding = '0';
+    nameplate.style.paddingLeft = `${nameplatePadding}px`;
+    nameplate.style.color = '#fff';
+    nameplate.style.backgroundColor = 'rgba(0,0,0,0)';
+    nameplate.style.textShadow = '0px 0px 5px black';
+    nameplate.style.letterSpacing = '0.1em';
+    nameplate.style.fontSize = `${nameplateSize - 6}px`;
+
+    let button = document.getElementById(`video-pause-${tileIndex}`) as HTMLButtonElement;
+
+    if (button) {
+      button.style.position = 'absolute';
+      button.style.display = 'inline-block';
+      button.style.right = '0px';
+      // button.style.top = `${h - nameplateSize - nameplatePadding}px`;
+      button.style.height = `${nameplateSize}px`;
+      // button.style.width = `${w}px`;
+      button.style.margin = '0';
+      button.style.padding = '0';
+      button.style.paddingLeft = `${nameplatePadding}px`;
+      button.style.color = '#fff';
+      button.style.backgroundColor = 'rgba(0,0,0,0)';
+      button.style.textShadow = '0px 0px 5px black';
+      button.style.letterSpacing = '0.1em';
+      button.style.fontSize = `${nameplateSize - 6}px`;
+    }
+
+    button = document.getElementById(`video-resume-${tileIndex}`) as HTMLButtonElement;
+
+    if (button) {
+      button.style.position = 'absolute';
+      button.style.left = '0px';
+      button.style.top = '0px';
+      button.style.height = `${nameplateSize}px`;
+      // button.style.width = `${w}px`;
+      button.style.margin = '0';
+      button.style.padding = '0';
+      button.style.paddingLeft = `${nameplatePadding}px`;
+      button.style.color = '#fff';
+      button.style.backgroundColor = 'rgba(0,0,0,0)';
+      button.style.textShadow = '0px 0px 5px black';
+      button.style.letterSpacing = '0.1em';
+      button.style.fontSize = `${nameplateSize - 6}px`;
+    }
+  }
+
+  layoutVideoTilesActiveSpeaker(visibleTileIndices: number[], activeTileId: number): void {
+    const tileArea = document.getElementById('tile-area') as HTMLDivElement;
+    const width = tileArea.clientWidth;
+    const height = tileArea.clientHeight;
+    const widthToHeightAspectRatio = 16 / 9;
+    const maximumRelativeHeightOfOthers = 0.3;
+
+    const activeWidth = width;
+    const activeHeight = width / widthToHeightAspectRatio;
+    const othersCount = visibleTileIndices.length - 1;
+    let othersWidth = width / othersCount;
+    let othersHeight = width / widthToHeightAspectRatio;
+    if (othersHeight / activeHeight > maximumRelativeHeightOfOthers) {
+      othersHeight = activeHeight * maximumRelativeHeightOfOthers;
+      othersWidth = othersHeight * widthToHeightAspectRatio;
+    }
+    if (othersCount === 0) {
+      othersHeight = 0;
+    }
+    const totalHeight = activeHeight + othersHeight;
+    const othersTotalWidth = othersWidth * othersCount;
+    const othersXOffset = width / 2 - othersTotalWidth / 2;
+    const activeYOffset = height / 2 - totalHeight / 2;
+    const othersYOffset = activeYOffset + activeHeight;
+
+    let othersIndex = 0;
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < visibleTileIndices.length; i++) {
+      const tileIndex = visibleTileIndices[i];
+      const tileId = this.tileIndexToTileId[tileIndex];
+      // tslint:disable-next-line: one-variable-per-declaration
+      let x = 0,
+        y = 0,
+        w = 0,
+        h = 0;
+      if (tileId === activeTileId) {
+        x = 0;
+        y = activeYOffset;
+        w = activeWidth;
+        h = activeHeight;
+      } else {
+        x = othersXOffset + othersIndex * othersWidth;
+        y = othersYOffset;
+        w = othersWidth;
+        h = othersHeight;
+        othersIndex += 1;
+      }
+      this.updateTilePlacement(tileIndex, x, y, w, h);
+    }
+  }
 }
 
 
@@ -308,5 +655,36 @@ class TestSound {
     new TimeoutScheduler((rampSec * 2 + durationSec + 1) * 1000).start(() => {
       audioContext.close();
     });
+  }
+}
+
+class DemoTileOrganizer {
+  private static MAX_TILES = 16;
+  private tiles: { [id: number]: number } = {};
+  public tileStates: { [id: number]: boolean } = {};
+
+  acquireTileIndex(tileId: number): number {
+    for (let index = 0; index < DemoTileOrganizer.MAX_TILES; index++) {
+      if (this.tiles[index] === tileId) {
+        return index;
+      }
+    }
+    for (let index = 0; index < DemoTileOrganizer.MAX_TILES; index++) {
+      if (!(index in this.tiles)) {
+        this.tiles[index] = tileId;
+        return index;
+      }
+    }
+    throw new Error('no tiles are available');
+  }
+
+  releaseTileIndex(tileId: number): number {
+    for (let index = 0; index < DemoTileOrganizer.MAX_TILES; index++) {
+      if (this.tiles[index] === tileId) {
+        delete this.tiles[index];
+        return index;
+      }
+    }
+    return DemoTileOrganizer.MAX_TILES;
   }
 }
